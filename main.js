7b4a3f1a798cfd032f8ce73454246c9edce30f06
dekersab3u <thomas.de-kersabiec1@etu.univ-lorraine.fr>
2025-03-18 18:04:50 +0100

    page MàJ

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const content = document.getElementById('content');
     const pages = {
         home: `
+            <div id="home">
             <h1>Suivi d'Alternance</h1>
-            <p>Suivi d'alternance de Mr. Thomas <nomdefamille> dans l'entreprise XXXX, 3ème année de BUT informatique à l'IUT Charlemagne à Nancy.</p>
+            <p><strong>Ce site présente le suivi d'alternance de Thomas DEKERSABIEC dans l'entreprise Bleuforet, 3ème année de BUT informatique à l'IUT Charlemagne à Nancy.</strong></p>
+            <p>Dans le cadre de ma 3ème année de BUT Informatique en option Réalisation d'Applications - Développement Web et mobile, je suis en alternance dans l'entreprise Bleuforêt, à Vagney dans les Vosges. Ce site présentera l'entreprise, le contexte dans lequel je mettrai en place les différents projets qui me seront confiés, puis présentera en détail les projets sur lequel je travaille, ce que j'ai réalisé...</p>
+            
+            <p><a href="#" data-page="entreprise">Présentation de l'entreprise Bleuforêt</a></p>
+            </div>
         `,
         entreprise: `
+            <div id="entreprise">
             <h1>L'Entreprise</h1>
-            <p>Présentation de l'entreprise et des personnes avec qui je travaille.</p>
+            <p></p>
+            </div>
         `,
         contexte: `
             <h1>Contexte</h1>
